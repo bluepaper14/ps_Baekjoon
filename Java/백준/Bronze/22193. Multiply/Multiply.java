c71c4a4b9341441e1f06dcccf8b595fcbf5db8ec
공지현 <83187050+bluepaper14@users.noreply.github.com>
2025-02-02 14:43:48 +0900

    [Bronze V] Title: Multiply, Time: 168 ms, Memory: 17648 KB, Score: 20 point -BaekjoonHub

---
+++ Java/백준/Bronze/22193. Multiply/Multiply.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        int sum = x * y;
+
+        System.out.println(sum);
+
+
+    }
+}
