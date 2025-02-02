9a6a3149cdfcb0dad196bfd7694e841f713c1c24
공지현 <83187050+bluepaper14@users.noreply.github.com>
2025-02-02 14:45:44 +0900

    [Bronze V] Title: Multiply, Time: 772 ms, Memory: 60788 KB, Score: 100 point -BaekjoonHub

--- Java/백준/Bronze/22193. Multiply/Multiply.java
+++ Java/백준/Bronze/22193. Multiply/Multiply.java
@@ -1,19 +1,25 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        long n = scanner.nextInt();
-        long m = scanner.nextInt();
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        scanner.nextLine();  // 개행 문자 제거 (nextInt() 후에는 nextLine()이 필요할 수 있음)
 
-        long x = scanner.nextInt();
-        long y = scanner.nextInt();
+        String a = scanner.nextLine();
+        String b = scanner.nextLine();
 
-        long sum = x * y;
+        // BigInteger를 사용하여 큰 수 연산 수행
+        BigInteger bigA = new BigInteger(a);
+        BigInteger bigB = new BigInteger(b);
+        BigInteger product = bigA.multiply(bigB);
 
-        System.out.println(sum);
+        // 결과 출력
+        System.out.println(product);
 
-
+        scanner.close();
     }
 }
