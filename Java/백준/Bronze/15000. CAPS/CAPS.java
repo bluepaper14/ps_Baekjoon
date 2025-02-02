57e0e8d513cbc81d1813c07c9e79a7903a91c61f
공지현 <83187050+bluepaper14@users.noreply.github.com>
2025-02-02 14:19:53 +0900

    [Bronze V] Title: CAPS, Time: 488 ms, Memory: 31360 KB -BaekjoonHub

---
+++ Java/백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        String answer = input.toUpperCase();
+
+        System.out.println(answer);
+
+    }
+}
