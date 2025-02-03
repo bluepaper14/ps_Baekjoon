9574cfd8138d4a9696639b408eb07af239dca30d
공지현 <83187050+bluepaper14@users.noreply.github.com>
2025-02-04 00:24:08 +0900

    [Bronze V] Title: Java Warriors, Time: 172 ms, Memory: 17588 KB -BaekjoonHub

---
+++ Java/백준/Bronze/30328. Java Warriors/Java Warriors.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+       int n = in.nextInt();
+
+       int m = n * 4000;
+
+       System.out.println(m);
+    }
+}
