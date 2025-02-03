e148a72ae1aff15a653d85fbb3289d06e9c7c8bd
공지현 <83187050+bluepaper14@users.noreply.github.com>
2025-02-03 16:25:11 +0900

    [Bronze V] Title: Micromasters, Time: 176 ms, Memory: 17676 KB -BaekjoonHub

---
+++ Java/백준/Bronze/33178. Micromasters/Micromasters.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int x = n / 10;
+        
+        System.out.println(x);
+    }
+}
