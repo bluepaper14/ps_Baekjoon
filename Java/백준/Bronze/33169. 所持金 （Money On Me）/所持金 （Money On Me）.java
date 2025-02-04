ad66731ee24c13c151a45750ee505382c115fe9e
공지현 <83187050+bluepaper14@users.noreply.github.com>
2025-02-04 14:01:58 +0900

    [Bronze V] Title: 所持金 (Money On Me), Time: 172 ms, Memory: 17736 KB -BaekjoonHub

---
+++ Java/백준/Bronze/33169. 所持金 （Money On Me）/所持金 （Money On Me）.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt() * 1000;
+        int m = sc.nextInt() * 10000;
+        System.out.println(m + n);
+    }
+
+}
+
