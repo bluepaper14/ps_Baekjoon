af491552fa6d2104f95f1b9a8bc7f6af7719cdd4
공지현 <83187050+bluepaper14@users.noreply.github.com>
2025-02-04 13:55:31 +0900

    [Bronze V] Title: Just Round Down, Time: 180 ms, Memory: 17824 KB -BaekjoonHub

---
+++ Java/백준/Bronze/32458. Just Round Down/Just Round Down.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+        
+        int x = (int) Math.floor(n);
+        System.out.println(x);
+
+
+    }
+}
+
