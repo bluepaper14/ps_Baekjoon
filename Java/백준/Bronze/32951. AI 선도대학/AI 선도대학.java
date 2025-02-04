eacfee7385b1d7c0f9a7e89b63d456b56a8cd4fd
공지현 <83187050+bluepaper14@users.noreply.github.com>
2025-02-04 13:34:33 +0900

    [Bronze V] Title: AI 선도대학, Time: 172 ms, Memory: 17700 KB -BaekjoonHub

---
+++ Java/백준/Bronze/32951. AI 선도대학/AI 선도대학.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+
+        int x = year - 2024;
+        System.out.println(x);
+    }
+}
+
