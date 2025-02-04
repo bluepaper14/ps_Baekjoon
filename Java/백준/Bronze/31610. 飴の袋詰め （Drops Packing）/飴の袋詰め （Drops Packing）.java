8eef367fc1ade21a94534b0c4d2407177e3c370c
공지현 <83187050+bluepaper14@users.noreply.github.com>
2025-02-04 13:23:59 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 172 ms, Memory: 17652 KB -BaekjoonHub

---
+++ Java/백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int h = sc.nextInt();
+        int w = sc.nextInt();
+        int x = sc.nextInt();
+
+        System.out.println(h * w + x);
+
+    }
+}
+
